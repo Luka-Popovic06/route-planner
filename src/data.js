0c056f2c668b16e4b4fb17e6292b448ecbf38f9c
Luka Popovic <lukapopovic0111@gmail.com>
2026-01-04 15:17:00 +0100

    add input config and travel mode options

---
+++ src/data.js
@@ -0,0 +1,19 @@
+export const InpArray = {
+  start: {
+    type: "text",
+    variation: "standard",
+    text: "Starting point",
+  },
+  stop: [],
+  destination: { type: "text", variation: "standard", text: "Destination" },
+};
+export const optionsArray = [
+  {
+    text: "Driving 🚗",
+    value: "driving",
+  },
+  {
+    text: "Walking 🚶‍♂️",
+    value: "walking",
+  },
+];
