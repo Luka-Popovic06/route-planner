834be9d5c83f8b6811f22abf6e686e6bfe58e5dc
Luka Popovic <lukapopovic0111@gmail.com>
2026-01-05 22:17:56 +0100

    Add Button component

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,1 +1,17 @@
-
+const Button = ({ children, btnType, variation, clickAction }) => {
+  const baseClass = "btn";
+  let btnClass;
+  if (variation === "standard") btnClass = "btn-standard";
+  if (variation === "standard-mini") btnClass = "btn-standard-mini";
+  if (variation === "primary") btnClass = "btn-primary";
+  return (
+    <button
+      type={btnType}
+      onClick={clickAction}
+      className={`${baseClass} ${btnClass}`}
+    >
+      {children}
+    </button>
+  );
+};
+export default Button;
